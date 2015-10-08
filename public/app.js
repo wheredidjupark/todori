870f9a8264d785f193bdd12b2ef4afde3c3b5bc7
Ju Hyeon Park <wheredidjupark@gmail.com>
2015-10-07 20:51:49 -0700

    erased the unnecessary controller

--- public/app.js
+++ public/app.js
@@ -1,48 +1,5 @@
-var app = angular.module("TodoApp", []);
+var app = angular.module("TodoApp", ['ui.router']);
 
-app.controller("TodoController", function($scope, $http, api) {
+app.config(function(){
 
-    $scope.todoName = "";
-    $scope.todoTask = "";
-
-    api.getTodo().then(function(data) {
-        $scope.todos = data;
-    });
-
-    $scope.removeTodo = function(todoId) {
-        api.removeTodo(todoId).then(function(data) {
-            $scope.todos = data;
-        });
-    };
-
-    $scope.postTodo = function() {
-        api.postTodo({name: $scope.todoName,task: $scope.todoTask }).then(function(data) {
-        	console.log(data);
-            $scope.todos = data;
-            $scope.todoName = "";
-            $scope.todoTask = "";
-        });
-    };
-
-/*
-    // $http.get("/api/todos/").then(function(response){
-    // 	$scope.todos = response.data;
-    // });
-
-    // $scope.removeTodo = function(todoId){
-    // 	$http.delete("/api/todos/"+todoId).then(function(response){
-    // 		$scope.todos = response.data;	
-    // 	});
-    // };
-
-
-
-    // $scope.post = function(){
-    // 	$http.post('/api/todos', {name: $scope.todoName, task: $scope.todoTask}).then(function(response){
-    // 		// console.log(response);
-    // 		$scope.todos = response.data;
-    // 	});
-    // 	$scope.todoName = "";
-    // };
-*/
 });
